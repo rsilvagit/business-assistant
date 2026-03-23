c68851a84359d26a40111c1d9ae6603f3625977a
Rafael Silva <rafael.sdasilva84@hotmail.com>
2026-03-22 23:36:33 -0300

    feat: alter service redis

--- src/BusinessAssistant.Api/Configurations/RedisConfiguration.cs
+++ src/BusinessAssistant.Api/Configurations/RedisConfiguration.cs
@@ -7,9 +7,11 @@ public static class RedisConfiguration
     public static IServiceCollection AddRedisConfiguration(this IServiceCollection services, IConfiguration configuration)
     {
         var redisConnectionString = configuration.GetConnectionString("Redis")!;
+        var options = ConfigurationOptions.Parse(redisConnectionString);
+        options.AbortOnConnectFail = false;
 
         services.AddSingleton<IConnectionMultiplexer>(
-            ConnectionMultiplexer.Connect(redisConnectionString));
+            ConnectionMultiplexer.Connect(options));
 
         return services;
     }
